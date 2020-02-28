22f759c7b673d7ef19527555fc61002da7a0a8d9
jimmyb2508 <james.barrington1985@gmail.com>
2020-02-28 23:03:15 +0000

    Cleaned up Code

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import QuoteMachine from './components/QuoteMachine';
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh',
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null,
+    }
+    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+    this.selectQuoteIndex = this.generateNewQuoteIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/jimmyb2508/b0601acd348790a3ac06aedf36e41885/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
+  }
+
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  /* Returns an integer representing an index in state.quotes */
+  /* If state.quotes is empty, returns undefined */
+
+  generateNewQuoteIndex() {
+    if (!this.state.quotes.length) {
+      return;
+    }
+    return random(0, this.state.quotes.length -1);
+  }
+
+  assignNewQuoteIndex() {
+    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
+  }
+
+render() {
+  console.log(this.state.selectedQuoteIndex);
+  return (
+    <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+      <Grid xs={11} lg={8} item>
+      {
+        this.selectedQuote ? <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} /> : 
+        null
+      }
+      </Grid>
+    </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
